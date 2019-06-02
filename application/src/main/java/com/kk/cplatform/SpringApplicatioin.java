4a5c6e3869f834f7e29aeaaa6da585f2f5dbf42d
kkorkk <kkorkk@foxmail.com>
2019-06-02 18:22:14 +0800

    [新增]初始化项目，建立项目的基本骨架，完成基本的登录页面和主页

---
+++ application/src/main/java/com/kk/cplatform/SpringApplicatioin.java
@@ -0,0 +1,18 @@
+package com.kk.cplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * SpringBoot 启动类
+ * @author kkorkk
+ * */
+
+@SpringBootApplication
+public class SpringApplicatioin{
+
+    public static void main(String[] args){
+        SpringApplication.run(SpringApplicatioin.class,args);
+    }
+
+}
---
+++ application/src/main/java/com/kk/cplatform/controller/IndexController.java
@@ -0,0 +1,24 @@
+package com.kk.cplatform.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @RequestMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @RequestMapping("test")
+    public String test(){
+        return "test";
+    }
+
+}
